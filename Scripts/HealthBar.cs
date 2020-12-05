8a85c84498fbbb2fb6cf9ee81370267e26faa40c
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-12-05 18:55:17 +0700

    Add files via upload

---
+++ Scripts/HealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;    
+
+    public void TakeDamage()
+    {
+        ControlSlider(-10);
+    }    
+
+    public void TakeHeal()
+    {
+        ControlSlider(10);
+    }
+
+    private void ControlSlider(int stepValue)
+    {
+        _slider.DOValue(_slider.value + stepValue, 4);
+    }
+}
